2aa2e826f7b9ea2d434217cc10ac7bc6d5e471cd
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2022-07-03 12:00:02 +0200

    feat: create a CatCard Component

---
+++ components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,13 @@
+import { ICatCard } from './CatCard';
+
+const base: ICatCard = {
+  tag: 'card',
+  title: `what's new in cats`,
+  body: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+  author: 'Wael',
+  time: '2h ago',
+};
+
+export const mockCatCardProps = {
+  base,
+};
