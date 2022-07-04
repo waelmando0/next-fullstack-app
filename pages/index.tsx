4c26a6bb15188370576772e7a33a1588557f22aa
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2022-07-04 11:32:30 +0200

    feat: make tailwind work with storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
-import BaseTemplate from '../components/templates/base/BaseTemplate';
-import { mockBaseTemplateProps } from '../components/templates/base/BaseTemplate.mocks';
-
-const Home = () => {
-  return (
-    <main className="flex-grow">
-      <div className="max-w-6xl mx-auto px-4 lg:px-6">
-        <div className="pt-24 pb-20">
-          <BaseTemplate {...mockBaseTemplateProps.base} />
-        </div>
-      </div>
-    </main>
-  );
+const home = () => {
+  return <div>home</div>;
 };
 
-export default Home;
+export default home;
